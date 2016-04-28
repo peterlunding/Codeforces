262dde6b7c57545911b027402f2b2bc0d9d8eeb6
peterlunding <plj@asseco.dk>
2016-04-28 09:00:07 +0200

    96A
    Football

---
+++ 96A/src/bytetest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class bytetest {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		byte n;
+		n = in.nextByte();
+		in.close();
+		System.out.println(n);
+	}
+}
